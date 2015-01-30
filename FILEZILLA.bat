231fb3435fce888d5b6972820f90af27a38acefa
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-01-30 16:15:39 +0100

    Create FILEZILLA.bat

---
+++ FILEZILLA.bat
@@ -0,0 +1,12 @@
+@echo off
+cls
+title Command Line (CL) - Filezilla
+
+echo Command Line (CL) - Filezilla Program
+echo (C) Copyright 2014-present Suriyaa Kudo
+echo.
+
+ver
+:cmd
+cd filezilla
+start filezilla.exe
